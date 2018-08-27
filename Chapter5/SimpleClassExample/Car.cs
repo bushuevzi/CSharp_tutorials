785efe277a98ca4bd74aa4733a3bb41b284456fb
bushuevzi@mail.ru <bushuevzi@mail.ru>
2018-08-27 21:42:18 +0300

    to project SimpleClassExample add constructors for class Car

---
+++ Chapter5/SimpleClassExample/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleClassExample
+{
+    class Car
+    {
+        //The 'state' of the Car
+        public string petName;
+        public int currSpeed;
+
+        //The functionality of the Car
+        //Using the expression-bodied member syntax 
+        public void PrintState() => Console.WriteLine($"{petName} is going {currSpeed} MPH.");
+
+        public void SpeedUp(int delta)
+            => currSpeed += delta;
+
+        // A custom default constructor
+        public Car()
+        {
+            petName = "Cuck";
+            currSpeed = 10;
+        }
+        //Here, currSpeed will receive the default of an int (zero)
+        public Car(string pn) => petName = pn;
+
+        //Let caller set the full state of the Car
+        public Car(string pn, int cs)
+        {
+            petName = pn;
+            currSpeed = cs;
+        }
+
+    }
+}
--- Chapter5/SimpleClassExample/Program.cs
+++ Chapter5/SimpleClassExample/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace SimpleClassExample
 {
@@ -10,6 +8,21 @@ namespace SimpleClassExample
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Class Types *****\n");
+
+            //Invoking the default constructor
+            Car chuck = new Car();
+            //prints "Chuck is going 10MPH"
+            chuck.PrintState();
+
+            //Make a Car called Mary going 0MPH
+            Car mary = new Car("Mary");
+            mary.PrintState();
+
+            //Make a Car called Daisy ging 75MPH
+            Car daisy = new Car("Daisy", 75);
+            daisy.PrintState();
+
         }
     }
 }
