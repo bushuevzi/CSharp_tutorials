7306bec40e1aad6f7f3d3eaa89931caff2a86337
bushuevzi@mail.ru <bushuevzi@mail.ru>
2018-09-10 23:54:04 +0300

    Закончен Chapter 8

---
+++ Chapter 8/ComparableCar/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ComparableCar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Make an array of Car objects
+            Car[] myAutos = new Car[5];
+            myAutos[0] = new Car("Rusty", 80, 1);
+            myAutos[1] = new Car("Mary", 40, 234);
+            myAutos[2] = new Car("Viper", 40, 34);
+            myAutos[3] = new Car("Mel", 40, 4);
+            myAutos[4] = new Car("Chucky", 40, 5);
+
+            //Display current array
+            Console.WriteLine("Here is the unordered set of cars:");
+            foreach(Car c in myAutos)
+                Console.WriteLine($"{c.CarID} {c.PetName}");
+
+            //Now, sort them using Icomparable!
+            Array.Sort(myAutos);
+            Console.WriteLine();
+
+            //Display sorted array
+            Console.WriteLine("Here is the ordered sete of cars:");
+            foreach(Car c in myAutos)
+                Console.WriteLine($"{c.CarID} {c.PetName}");
+            Console.WriteLine();
+            /////////////////////////////////////////////////////////////////////////////////////////////
+            //Now sort by pet name 
+            Array.Sort(myAutos, new PetNameComparer());
+
+            //Dump sorted array
+            Console.WriteLine("Ordering by pet name:");
+            foreach(Car c in myAutos)
+                Console.WriteLine($"{c.CarID} {c.PetName}");
+        }
+    }
+}
---
+++ Chapter 8/CustomEnumerator/PetNameComparer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+
+namespace CustomEnumerator
+{
+    //This helper class is used to sort an array of Cars by pet name
+    class PetNameComparer : IComparer
+    {
+        //Test the pet name of each object
+        int IComparer.Compare(object x, object y)
+        {
+            Car t1 = x as Car;
+            Car t2 = y as Car;
+            if (t1 != null && t2 != null)
+                return String.Compare(t1.PetName, t2.PetName);
+            else
+                throw new ArgumentException("Parameter is not a Car!");
+        }
+
+    }
+}
