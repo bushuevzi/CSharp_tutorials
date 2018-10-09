56aab41dc4434f75c201ce03de23c3520b253e3d
bushuevzi <bushuevzi@mail.ru>
2018-10-09 17:16:59 +0300

    Revert "Р”РѕР±Р°РІР»РµРЅ РїСЂРёРјРµСЂ СЂР°Р±РѕС‚С‹ СЃ Excel"
    This reverts commit 9516cbde6eb4eea3bbf2fc42f7d4d50e2065788b.

--- Chapter_9/IssuesWithNonGenericCollections/Program.cs
+++ Chapter_9/IssuesWithNonGenericCollections/Program.cs
@@ -49,6 +49,25 @@ namespace IssuesWithNonGenericCollections
         }
     }
 
+    public class Person
+    {
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person() { }
+        public Person(string firstName, string lastName, int age)
+        {
+            Age = age;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public override string ToString()
+        {
+            return $"Name: {FirstName} {LastName}, Age: {Age}";
+        }
+    }
+
     public class PersonCollection : IEnumerable
     {
         private ArrayList arPeople = new ArrayList();
--- Chapter_9/SimpleGeneric/Program.cs
+++ Chapter_9/SimpleGeneric/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections;
 
 namespace SimpleGeneric
 {
@@ -8,24 +7,6 @@ namespace SimpleGeneric
     {
         static void Main(string[] args)
         {
-            int[] myInts = { 10, 4, 2, 33, 93 };
-            //Specify the placeholder to the generic Sort<>() method
-            Array.Sort<int>(myInts);
-            foreach(int i in myInts)
-                Console.WriteLine(i);
-        }
-
-        static void InitialisationCollections()
-        {
-            //Init standard array
-            int[] myArrayOfInts = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-            //Init a generic List<> of ints
-            List<int> myGenericList = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-            //Init an Array List with numerical data
-            ArrayList myList = new ArrayList { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            
         }
 
         static void UseGenericList()
